e65966226532edd9e9aba0af09d004a6d8297adb
Vadim <vadimrembyboy@gmail.com>
2023-03-30 23:34:40 +0300

    help

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.first_page, name='first_page'),
     path('about-me/', views.second_page, name='second_page'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
+from django.utils import timezone
 
-# Create your views here.
+
 def first_page(request):
     return render(request, 'blog/first_page.html')
 
@@ -13,3 +16,16 @@ def second_page(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
